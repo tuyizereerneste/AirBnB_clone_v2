33a06efaa7237eca4d1b2f1f4bb60728b5a7d42a
tuyizereerneste <tuyizereernest@yahoo.com>
2023-12-08 01:12:04 +0200

    Fabric script that generates a .tgz archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Fabric script that generates a .tgz archive from the contents of the web_static
+# Fabric script that generates a .tgz archive
+# from the contents of the web_static
 
 from datetime import datetime
 from fabric.api import local
