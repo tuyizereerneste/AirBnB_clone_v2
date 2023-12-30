b3a2290c6809d78eeaf2bbfe7a225e7ab4759177
tuyizereerneste <tuyizereernest@yahoo.com>
2023-12-30 18:30:03 +0200

    Hello HBNB

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application
+on 0.0.0.0, port 5000, Routes: /: display “Hello HBNB!”
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
