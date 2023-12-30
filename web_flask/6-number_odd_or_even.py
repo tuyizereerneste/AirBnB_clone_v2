a7a84a1e00de207c695275c9dd7c20f0e96b46bc
tuyizereerneste <tuyizereernest@yahoo.com>
2023-12-30 20:36:24 +0200

    even or odd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -56,7 +56,8 @@ def even_or_odd(n):
         answer = "even"
     else:
         answer = "odd"
-    return render_template("6-number_odd_or_even.html", number=n, result=answer)
+    return render_template("6-number_odd_or_even.html",
+                           number=n, result=answer)
 
 
 if __name__ == "__main__":
