18b801ab78bd6715b9c38cb1a4c649883af0cbf8
tuyizereerneste <tuyizereernest@yahoo.com>
2023-12-30 18:57:42 +0200

    C route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Flask web application must be listening on 0.0.0.0
+Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+    /c/<text>: display “C ” followed by the value of the text
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    word = text.replace("_", " ")
+    return "C {}".format(word)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
